65cb7da20d18c7018215b2150365f914657eaa04
Owner <Askndr312@gmail.com>
2019-09-22 14:17:10 +0300

    Initial commit

---
+++ app/src/main/java/kersh/wael/waelkersh/project11.java
@@ -0,0 +1,14 @@
+package kersh.wael.waelkersh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class project11 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project11);
+        //
+    }
+}
