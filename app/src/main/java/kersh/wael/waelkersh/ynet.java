cc906f041b2eca9403d5fc04e6125b0c9b6bc8fc
Owner <Askndr312@gmail.com>
2019-09-22 14:54:37 +0300

    kk

---
+++ app/src/main/java/kersh/wael/waelkersh/ynet.java
@@ -0,0 +1,13 @@
+package kersh.wael.waelkersh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ynet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ynet);
+    }
+}
